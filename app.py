a29b83723850d71dffd8fb0697c295f2b48ced97
Amaan MK <syedkhalander66@gmail.com>
2024-08-30 22:30:32 +0530

    chore: lack

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ app = Flask(__name__)
 CORS(app)
 from routes.email_routes import email_routes
 from routes.text_routes import text_routes
-from routes.video_routes import video_routes
+# from routes.video_routes import video_routes
 # from routes.face_routes import face_routes
 from routes.files_routes import files_routes
 
-app.register_blueprint(video_routes)
+# app.register_blueprint(video_routes)
 app.register_blueprint(email_routes)
 app.register_blueprint(text_routes)
 # app.register_blueprint(face_routes)
