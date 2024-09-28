26ce922a59b5bd1262db63d055beecbeb34d77f0
Amaan Mohammad Khalander <syedkhalander66@gmail.com>
2024-09-28 13:22:57 +0530

    feat: new requirements

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 # CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "http://localhost:5000"]}})
 
 CORS(app)
-from routes.email_routes import email_routes
-from routes.text_routes import text_routes
+from wxm_routes.email_routes import email_routes
+from wxm_routes.text_routes import text_routes
+from wxm_routes.files_routes import files_routes
 # from routes.video_routes import video_routes
 # from routes.face_routes import face_routes
-from routes.files_routes import files_routes
 
 # app.register_blueprint(video_routes)
 app.register_blueprint(email_routes)
